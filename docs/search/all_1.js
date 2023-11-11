e072b20877f3d2d9b51dbba30ed4c61e7b377adc
Gernot Neppert <Gernot.Neppert@plath.de>
2023-11-11 13:25:28 +0100

    Updated Documentation.

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['delegategetregistration_0',['delegateGetRegistration',['../classmcnepp_1_1qtdi_1_1_q_application_context.html#a0b97f9cd7f7eb0dbb2b7a15df5fac978',1,'mcnepp::qtdi::QApplicationContext']]],
+  ['delegategetregistration_0',['delegateGetRegistration',['../classmcnepp_1_1qtdi_1_1_q_application_context.html#a7e5dfff61c69a7b432dc68e15d39f207',1,'mcnepp::qtdi::QApplicationContext']]],
   ['delegateregisterobject_1',['delegateRegisterObject',['../classmcnepp_1_1qtdi_1_1_q_application_context.html#adb7e4b80512a2c20fe3cdccd6faee649',1,'mcnepp::qtdi::QApplicationContext']]],
   ['delegateregisterservice_2',['delegateRegisterService',['../classmcnepp_1_1qtdi_1_1_q_application_context.html#a7f870a9d5acb4bc7e7b35c105a826f5d',1,'mcnepp::qtdi::QApplicationContext']]],
   ['dependency_3',['Dependency',['../structmcnepp_1_1qtdi_1_1_dependency.html',1,'mcnepp::qtdi']]],
