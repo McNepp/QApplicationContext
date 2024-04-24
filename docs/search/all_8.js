2bee321b382cf1f4e45b3716984d67c472e27c9c
Gernot Neppert <Gernot.Neppert@plath.de>
2024-04-24 19:56:26 +0200

    Updated Doxygen.

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['make_5fconfig_0',['make_config',['../qapplicationcontext_8h.html#af9b302c5ac054bc52b9ad0a7721ceed5',1,'mcnepp::qtdi']]],
   ['mandatory_1',['MANDATORY',['../qapplicationcontext_8h.html#aa26d711edb47348796326730adbbc521ac25db15b6f91b287b5b3c8a515bd21ce',1,'mcnepp::qtdi']]],
-  ['matches_2',['matches',['../classmcnepp_1_1qtdi_1_1_registration.html#a4562640be6a8d1f377411c0ab912c34f',1,'mcnepp::qtdi::Registration::matches()'],['../qapplicationcontext_8h.html#ad99aa9332aee20a47057f62ed4e8f285',1,'mcnepp::qtdi::matches(registration_handle_t handle)']]],
-  ['matches_3c_20qobject_20_3e_3',['matches&lt; QObject &gt;',['../qapplicationcontext_8h.html#af2e530b1413a562c467126d14058b73a',1,'mcnepp::qtdi']]]
+  ['matches_2',['matches',['../classmcnepp_1_1qtdi_1_1_registration.html#a4562640be6a8d1f377411c0ab912c34f',1,'mcnepp::qtdi::Registration::matches()'],['../qapplicationcontext_8h.html#ad99aa9332aee20a47057f62ed4e8f285',1,'mcnepp::qtdi::matches()']]]
 ];
--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -10,10 +10,11 @@ var searchData=
   ['service_5fregistration_5fhandle_5ft_7',['service_registration_handle_t',['../qapplicationcontext_8h.html#a4db8ccaad83e16ecef8cf25908b5d712',1,'mcnepp::qtdi']]],
   ['service_5ftraits_8',['service_traits',['../structmcnepp_1_1qtdi_1_1service__traits.html',1,'mcnepp::qtdi']]],
   ['servicefactory_9',['serviceFactory',['../qapplicationcontext_8h.html#abf9595ac2cbf31caa9402aada1b87490',1,'mcnepp::qtdi']]],
-  ['serviceregistration_10',['ServiceRegistration',['../classmcnepp_1_1qtdi_1_1_service_registration.html',1,'mcnepp::qtdi']]],
-  ['servicescope_11',['ServiceScope',['../qapplicationcontext_8h.html#a5429ea6117bd8ce9da93bb56b0affded',1,'mcnepp::qtdi']]],
-  ['standardapplicationcontext_12',['StandardApplicationContext',['../classmcnepp_1_1qtdi_1_1_standard_application_context.html',1,'mcnepp::qtdi']]],
-  ['subscribe_13',['subscribe',['../classmcnepp_1_1qtdi_1_1_registration.html#aa70f34088d747d139cf12f9e7e02df8f',1,'mcnepp::qtdi::Registration::subscribe(QObject *context, F callable, Qt::ConnectionType connectionType=Qt::AutoConnection)'],['../classmcnepp_1_1qtdi_1_1_registration.html#a19a7323c8bad9818df4583f5268c0ebf',1,'mcnepp::qtdi::Registration::subscribe(T *target, R(T::*setter)(S *), Qt::ConnectionType connectionType=Qt::AutoConnection)']]],
-  ['subscription_14',['Subscription',['../classmcnepp_1_1qtdi_1_1_subscription.html',1,'mcnepp::qtdi']]],
-  ['subscription_5fhandle_5ft_15',['subscription_handle_t',['../qapplicationcontext_8h.html#a3b06c799b9c4a19b40ee9213b2715d41',1,'mcnepp::qtdi']]]
+  ['servicemetaobject_10',['serviceMetaObject',['../qapplicationcontext_8h.html#a4d48e0805aa7f293c0b050534a8c6e9e',1,'mcnepp::qtdi']]],
+  ['serviceregistration_11',['ServiceRegistration',['../classmcnepp_1_1qtdi_1_1_service_registration.html',1,'mcnepp::qtdi']]],
+  ['servicescope_12',['ServiceScope',['../qapplicationcontext_8h.html#a5429ea6117bd8ce9da93bb56b0affded',1,'mcnepp::qtdi']]],
+  ['standardapplicationcontext_13',['StandardApplicationContext',['../classmcnepp_1_1qtdi_1_1_standard_application_context.html',1,'mcnepp::qtdi']]],
+  ['subscribe_14',['subscribe',['../classmcnepp_1_1qtdi_1_1_registration.html#aa70f34088d747d139cf12f9e7e02df8f',1,'mcnepp::qtdi::Registration::subscribe(QObject *context, F callable, Qt::ConnectionType connectionType=Qt::AutoConnection)'],['../classmcnepp_1_1qtdi_1_1_registration.html#a19a7323c8bad9818df4583f5268c0ebf',1,'mcnepp::qtdi::Registration::subscribe(T *target, R(T::*setter)(S *), Qt::ConnectionType connectionType=Qt::AutoConnection)']]],
+  ['subscription_15',['Subscription',['../classmcnepp_1_1qtdi_1_1_subscription.html',1,'mcnepp::qtdi']]],
+  ['subscription_5fhandle_5ft_16',['subscription_handle_t',['../qapplicationcontext_8h.html#a3b06c799b9c4a19b40ee9213b2715d41',1,'mcnepp::qtdi']]]
 ];
