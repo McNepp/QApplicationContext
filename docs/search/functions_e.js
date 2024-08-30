8f151b488ce8e47dece449483e04e00349c54556
Gernot Neppert <Gernot.Neppert@plath.de>
2024-08-30 17:47:00 +0200

    Upated Doxygen.

--- docs/search/functions_e.js
+++ docs/search/functions_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['wrap_0',['wrap',['../classmcnepp_1_1qtdi_1_1_service_registration.html#a0f9678445bc2936fb7dd510331f7c302',1,'mcnepp::qtdi::ServiceRegistration::wrap()'],['../classmcnepp_1_1qtdi_1_1_proxy_registration.html#a1bfc50440aeeb7048a8ef1bda4df9e17',1,'mcnepp::qtdi::ProxyRegistration::wrap()']]]
+  ['withinit_0',['withInit',['../structmcnepp_1_1qtdi_1_1_service.html#acfe0d6d931c2087dc9913dffbbde28dd',1,'mcnepp::qtdi::Service::withInit(I initializer) &amp;&amp;'],['../structmcnepp_1_1qtdi_1_1_service.html#a88b239b9b0739b3d5fe9384b94f892ab',1,'mcnepp::qtdi::Service::withInit(I initializer) const &amp;']]],
+  ['wrap_1',['wrap',['../classmcnepp_1_1qtdi_1_1_service_registration.html#a0f9678445bc2936fb7dd510331f7c302',1,'mcnepp::qtdi::ServiceRegistration::wrap()'],['../classmcnepp_1_1qtdi_1_1_proxy_registration.html#a1bfc50440aeeb7048a8ef1bda4df9e17',1,'mcnepp::qtdi::ProxyRegistration::wrap()']]]
 ];
--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['autowire_0',['autowire',['../structmcnepp_1_1qtdi_1_1service__config.html#a46ccc53f5141f5e25a765d5014c7ece5',1,'mcnepp::qtdi::service_config']]]
+  ['autorefresh_0',['autoRefresh',['../structmcnepp_1_1qtdi_1_1service__config.html#a19d691aa3807885a3e3f60f91b02d81d',1,'mcnepp::qtdi::service_config']]],
+  ['autowire_1',['autowire',['../structmcnepp_1_1qtdi_1_1service__config.html#a46ccc53f5141f5e25a765d5014c7ece5',1,'mcnepp::qtdi::service_config']]]
 ];
